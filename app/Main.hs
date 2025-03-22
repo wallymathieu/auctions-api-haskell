c7f435ccdd2e9373722109b85eab067eca607b09
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2025-03-22 21:54:33 +0200

    +Event serialization

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,17 @@ module Main where
 import           Data.Time (getCurrentTime)
 import           Web.Spock
 import           Web.Spock.Config
+import qualified Data.Map as Map
 import           AuctionSite.Web.App
+import           AuctionSite.Domain.Commands
+import           AuctionSite.Persistence.JsonFile
 
+eventsFile = "events.json"
+onEvent :: Event -> IO ()
+onEvent e = writeEvents eventsFile [e]
 main :: IO ()
 main = do
-  state <- initAppState
+  events <- readEvents eventsFile >>= maybe (return []) return
+  state <- initAppState $ eventsToAuctionStates events
   spockCfg <- defaultSpockCfg () PCNoDatabase state
-  runSpock 8080 (spock spockCfg (app getCurrentTime))
+  runSpock 8080 (spock spockCfg (app onEvent getCurrentTime))
--- src/AuctionSite/Domain/Commands.hs
+++ src/AuctionSite/Domain/Commands.hs
@@ -1,21 +1,41 @@
 {-# LANGUAGE DeriveGeneric,OverloadedStrings   #-}
 module AuctionSite.Domain.Commands where
-import AuctionSite.Domain.Auctions
-import AuctionSite.Domain.Bids
-import GHC.Generics
-import Data.Time
-import Data.Aeson
+import           AuctionSite.Domain.Auctions
+import           AuctionSite.Domain.Bids
+import           AuctionSite.Domain.Core
+import           AuctionSite.Domain.States
+
+import           GHC.Generics
+import           Data.Time
+import           Data.Aeson
+import qualified Data.Map as Map
+
 
 data Command =
   AddAuction UTCTime Auction
   | PlaceBid UTCTime Bid
   deriving (Generic, Show, Eq)
 
-data CommandSuccess =
+data Event =
   AuctionAdded UTCTime Auction
   | BidAccepted UTCTime Bid
   deriving (Generic, Show, Eq)
 
+-- | Fold to map of auction that contains both the auction and state
+eventsToAuctionStates :: [Event] -> Map.Map AuctionId (Auction, AuctionState)
+eventsToAuctionStates = foldl folder Map.empty
+  where
+    folder :: Map.Map AuctionId (Auction, AuctionState) -> Event -> Map.Map AuctionId (Auction, AuctionState)
+    folder auctions event = case event of
+      AuctionAdded _ auction ->
+        let empty = emptyState auction
+        in Map.insert (auctionId auction) (auction, empty) auctions
+      BidAccepted _ b ->
+        case Map.lookup (forAuction b) auctions of
+          Just (auction, state) ->
+            let (next, _) = addBid b state
+            in Map.insert (forAuction b) (auction, next) auctions
+          Nothing -> error "could not find auction"
 
 instance ToJSON Command where
   toJSON (AddAuction time auction) =  object ["$type" .= String "AddAuction", "at" .= time, "auction" .= auction]
@@ -30,6 +50,22 @@ instance FromJSON Command where
       _                   -> fail "Unknown command type"
   parseJSON _ = fail "Unexpected json command"
 
-instance ToJSON CommandSuccess where
-  toJSON (AuctionAdded time auction) =  object ["$type" .= String "AuctionAdded", "at" .= time, "auction" .= auction]
-  toJSON (BidAccepted time bid) =  object ["$type" .= String "BidAccepted", "at" .= time, "bid" .= bid]
+instance ToJSON Event where
+  toJSON (AuctionAdded time auction) = object ["$type" .= String "AuctionAdded", "at" .= time, "auction" .= auction]
+  toJSON (BidAccepted time bid) = object ["$type" .= String "BidAccepted", "at" .= time, "bid" .= bid]
+
+instance FromJSON Event where
+  parseJSON = withObject "Event" $ \obj -> do
+    eventType <- obj .: "$type"
+    case eventType of
+      "AuctionAdded" -> do
+        at' <- obj .: "at"
+        auction <- obj .: "auction"
+        return $ AuctionAdded at' auction
+
+      "BidAccepted" -> do
+        at' <- obj .: "at"
+        bid <- obj .: "bid"
+        return $ BidAccepted at' bid
+
+      _ -> fail $ "Unknown event type: " ++ eventType
